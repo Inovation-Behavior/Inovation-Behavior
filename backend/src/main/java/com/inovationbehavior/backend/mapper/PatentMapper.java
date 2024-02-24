4abbe1a9d59f9d46d51704023f1dcffa599b0e88
litianlololo <102678203+litianlololo@users.noreply.github.com>
2024-02-24 11:10:38 +0800

    后端 获取公司全名+获取候选专利号接口

--- backend/src/main/java/com/inovationbehavior/backend/mapper/PatentMapper.java
+++ backend/src/main/java/com/inovationbehavior/backend/mapper/PatentMapper.java
@@ -4,6 +4,9 @@ import com.inovationbehavior.backend.model.Patent;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface PatentMapper {
     @Select("""
@@ -11,4 +14,18 @@ public interface PatentMapper {
     WHERE No = #{No}
     """)
     Patent getPatentByNo(String No);
+
+    @Select("""
+    SELECT DISTINCT application FROM patent 
+    WHERE application LIKE CONCAT('%',#{key},'%')
+    """)
+    List<String> getCompanyByKey(String Key);
+
+    @Select("""
+    SELECT no,name, application FROM patent
+    WHERE application LIKE CONCAT('%', #{company}, '%')
+    AND no LIKE CONCAT('%', #{no}, '%')
+""")
+    List<Map<String, String>> getPatentByKey(String company, String no);
+
 }
