f835417692d9d69649655215ee9c8820d5652ae9
litianlololo <102678203+litianlololo@users.noreply.github.com>
2024-02-26 17:24:36 +0800

    spring aop实操之打印日志

--- backend/src/main/java/com/inovationbehavior/backend/controller/PatentController.java
+++ backend/src/main/java/com/inovationbehavior/backend/controller/PatentController.java
@@ -1,5 +1,6 @@
 package com.inovationbehavior.backend.controller;
 
+import com.inovationbehavior.backend.annotation.LogAnnotation;
 import com.inovationbehavior.backend.model.Patent;
 import com.inovationbehavior.backend.model.Result;
 import com.inovationbehavior.backend.service.intf.PatentService;
@@ -17,6 +18,7 @@ public class PatentController {
     @Autowired
     PatentService patentService;
 
+    @LogAnnotation("根据No号查询详情")
     @GetMapping("/{No}")
     public Result getPatentByNo(@PathVariable String No) {
         Patent tmp = patentService.getPatentByNo(No);
