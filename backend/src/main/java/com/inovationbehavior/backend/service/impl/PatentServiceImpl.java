a3c1c3ce5c4b86431248a3c2aeb0b9baa8e72eba
litianlololo <102678203+litianlololo@users.noreply.github.com>
2024-03-01 20:59:46 +0800

    引入redis，缓存专利信息，读取速度80ms->30ms，赢

--- backend/src/main/java/com/inovationbehavior/backend/service/impl/PatentServiceImpl.java
+++ backend/src/main/java/com/inovationbehavior/backend/service/impl/PatentServiceImpl.java
@@ -1,14 +1,18 @@
 package com.inovationbehavior.backend.service.impl;
 
+import com.google.gson.Gson;
 import com.inovationbehavior.backend.mapper.PatentMapper;
 import com.inovationbehavior.backend.model.Patent;
 import com.inovationbehavior.backend.service.intf.PatentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -16,9 +20,36 @@ public class PatentServiceImpl implements PatentService {
     @Autowired
     private PatentMapper patentMapper;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Override
-    public Patent getPatentByNo(String No){
-        return patentMapper.getPatentByNo(No);
+    public Patent getPatentByNo(String no){
+        // Redis的key
+        String redisKey = "patent:" + no;
+
+        // 尝试从Redis中获取记录
+        String patentJson = redisTemplate.opsForValue().get(redisKey);
+        Patent patent = null;
+        Gson gson = new Gson();
+        if (patentJson != null && !patentJson.isEmpty()) {
+            // 如果Redis中有记录，反序列化后返回
+            patent = gson.fromJson(patentJson, Patent.class);
+        } else {
+            // Redis中没有记录，查询数据库
+            patent = patentMapper.getPatentByNo(no);
+
+            if (patent != null) {
+                // 数据库中查询到了记录，序列化后存储到Redis中
+                redisTemplate.opsForValue().set(redisKey, gson.toJson(patent));
+            }else{
+                //设置null值
+                redisTemplate.opsForValue().set(redisKey,gson.toJson(null));
+                redisTemplate.expire(redisKey,24, TimeUnit.HOURS);
+            }
+        }
+
+        return patent;
     }
 
     @Override
