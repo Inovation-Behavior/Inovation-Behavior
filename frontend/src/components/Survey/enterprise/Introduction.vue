56d63c7048beecf04b2ff97a66b73bd35534e1ad
Xie_yi <103160912+HeartFaker@users.noreply.github.com>
2024-03-31 17:24:52 +0800

    submit 0331 查询专利

--- frontend/src/components/Survey/enterprise/Introduction.vue
+++ frontend/src/components/Survey/enterprise/Introduction.vue
@@ -28,11 +28,28 @@
             </p>
             <p style="text-indent: 2em;">
                 （专利申请号为：
-                <el-input size="small" style="width: 5vw;" v-model="form.patentNo" @input="updatePatentNo"></el-input>
+                <el-input size="small" style="width: 7vw;" v-model="form.patentNo" @input="updatePatentNo"
+                    @keyup.enter="getPatentByNo(form.patentNo)"></el-input>
                 ）。如果与该专利无关，请提供您所在单位的名称：
-                <el-input size="small" style="width: 5vw;" v-model="form.companyName" @input="updateCompanyName"></el-input>
+                <el-input size="small" style="width: 7vw;" v-model="form.companyName" @input="updateCompanyName"
+                    @keyup.enter="getPatentNoByCompany(form.companyName)"></el-input>
                 ，我们将为您推荐其他专利。
             </p>
+            <template v-if="patentDetail.no!=''">
+                <div v-if="patentDetail.name">
+                    <p><strong>专利名称:</strong> {{ patentDetail.name }}</p>
+                    <p><strong>专利概述:</strong> {{ patentDetail.summary }}</p>
+                    <p><strong>专利链接:</strong> <a :href="patentDetail.pdfs[0]" target="blank">{{ patentDetail.pdfs[0]
+                            }}</a></p>
+                </div>
+            </template>
+            <template v-if="searchPatents.length!=0">
+                <el-table :data="searchPatents" style="width: 100%">
+                    <el-table-column prop="no" label="no" width="180" />
+                    <el-table-column prop="application" label="application" width="200" />
+                    <el-table-column prop="name" label="name" />
+                </el-table>
+            </template>
         </el-card>
         <p style="color: rgb(0,112,192)">
             填写形式：
@@ -52,6 +69,8 @@
 <script setup>
 import { ref, reactive } from 'vue';
 import { surveyStore } from '../../../stores/survey';
+import axios from 'axios';
+import { ElMessage } from 'element-plus';
 const surveyInfo = surveyStore().surveyInfo
 const updatePatentNo = (value) => {
     surveyInfo.patentNo = value;
@@ -63,6 +82,43 @@ const form = reactive({
     patentNo:"",
     companyName:""
 });
+
+const patentDetail = ref({});
+const getPatentByNo = async (no) => {
+    if (no == '') {
+        return;
+    }
+    let response = await axios.get('api/patents/' + no);
+    if(response.data.code == 0){
+        ElMessage.error("抱歉，专利号有误或专利不存在")
+    }else if(response.data.code ==1){
+        patentDetail.value = response.data.data
+    }
+}
+
+const searchPatents = ref([]);
+const getPatentNoByCompany = async (name) => {
+    try {
+        const no = "";
+        const response = await axios.get(`api/patents`, {
+            params: {
+                company: name,
+                no: ""
+            }
+        });
+        if (response.data.code == 0) {
+            ElMessage.error("抱歉，搜索失败")
+        } else if (response.data.code == 1) {
+            searchPatents.value = response.data.data.slice(0, 10)
+            console.log(searchPatents.value)
+        }
+        return;
+    } catch (error) {
+        // 处理请求失败的情况
+        console.error('请求失败:', error);
+        throw error; // 可以选择抛出错误以供调用者处理
+    }
+}
 </script>
 
 
